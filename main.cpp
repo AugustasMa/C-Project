abaa17412391cab7f260ad6053322fc1ea589b17
AugustasMa <113709620+AugustasMa@users.noreply.github.com>
2022-09-16 19:30:47 +0300

    Add files via upload
    Darbas su vektoriais

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <bits/stdc++.h>
-
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
 using namespace std;
+
 struct vaikas{
-string vard; // vardas
-string pav; // pavarde
-int egz; // egzamino rez.
-int ndkiekis = 0; //namu darbu pazymiu kiekis
-int *ndrez; // namu darbu rezultatu masyvas
+    string vard;
+    string pav;
+    int egz; // egzamino rez.
+    int ndkiekis = 0; // namu darbu pazymiu kiekis
+    vector<int> ndrez;// namų darbų rez.
 };
-
-
-void ivedimas(vaikas Masyvas[], int n) // skaitymo funkcija
+void ivedimas(vaikas Masyvas[], int n)
 {
     int nd; // konkretaus namu darbo rezultatas
-    int *ndrez2; // laikinas masyvas talpos padidinimui
     for(int i=0; i<n; i++){
         cout << "Iveskite vaiko varda: ";
         cin >> Masyvas[i].vard;
@@ -22,39 +23,43 @@ void ivedimas(vaikas Masyvas[], int n) // skaitymo funkcija
         cin >> Masyvas[i].pav;
         cout << "Iveskite vaiko egzamino rezultata: ";
         cin >> Masyvas[i].egz;
-        while(true) // ciklas, kurio metu duomenys yra saugomi vienam dinaminiam masyve ir perduodami kitam.
-        {
-        cout << "Iveskite namu darbo pazymi, jeigu daugiau pazymiu nera, iveskite -1: " << endl;
-        cin >> nd;
-        if(nd == -1){
-            break;
-            }
-        Masyvas[i].ndkiekis += 1;
-        ndrez2 = new int(Masyvas[i].ndkiekis);
-        if(Masyvas[i].ndkiekis != 1){
-            for(int j=0; j<Masyvas[i].ndkiekis-1; j++)
+        string pasirinkti;
+        cout << "Pasirinkite ar nau darbu rezultatus ivedinesite ranka, ar juos sugeneruos kompiuteris? Jeigu ivedinesite ranka rasykite: taip, jei tai atliks kompiuteris, rasykite: ne" << endl;
+        cin >> pasirinkti;
+        if(pasirinkti == "taip"){
+            while(true) // ivesti arba randominiai skaiciai sukeliami i vektoriaus pabaiga
             {
-                ndrez2[j+1] = Masyvas[i].ndrez[j];
+            cout << "Iveskite namu darbo pazymi, jeigu daugiau pazymiu nera, iveskite -1: " << endl;
+            cin >> nd;
+            if(nd == -1){
+                break;
+                }
+            Masyvas[i].ndkiekis += 1;
+            Masyvas[i].ndrez.push_back(nd);
             }
-            delete[] Masyvas[i].ndrez;
+            if(Masyvas[i].ndkiekis==0)
+            {
+                cout << "Mokinys neturi nei vieno namu darbu pazymio" << endl;
+            }
         }
-        ndrez2[0] = nd;
-        Masyvas[i].ndrez = ndrez2;
-        }
-        if(Masyvas[i].ndkiekis==0) // bug'o tikrinimas
+        else
         {
-            cout << "Mokinys neturi nei vieno namu darbu pazymio" << endl;
+
+            for(int z=0; z<(rand()%10+1); z++){
+            Masyvas[i].ndkiekis += 1;
+            Masyvas[i].ndrez.push_back(rand()%10+1);
+            }
+            }
         }
-    }
 }
-int sum(int labas[], int n){ // funkcija skaiciuojanti visu namu darbu suma
+int sum(vector<int> labas, int n){ // sumos funkcija
     int visindrez = 0;
     for(int i=0; i<n; i++){
         visindrez += labas[i];
     }
     return visindrez;
 }
-double vidurkis(double visindrez, int m)
+double vidurkis(double visindrez, int m) // vidurkio funkcija
 {
     double vid;
     vid = visindrez/m;
@@ -62,7 +67,7 @@ double vidurkis(double visindrez, int m)
         vid = 0;
     return vid;
 }
-double median(vaikas Masyvas, int n){ // funkcija medianos radimui
+double median(vaikas Masyvas, int n){ // medianos funkcija
     double mediana;
     if(Masyvas.ndkiekis==0){
         mediana=0;
@@ -79,15 +84,6 @@ double median(vaikas Masyvas, int n){ // funkcija medianos radimui
     return mediana;
 }
 
-void rusiavimas(vaikas Masyvas){ // ciklo cikle pagalba atliekama rusiavimo funkcija, kurios metu mazesne,
-                                //is dvieju lyginamu reiksmiu, yra perkeliama i kairesne pozicija.
-    for(int i=0; i<Masyvas.ndkiekis-1; i++){
-        for(int j=i;j<Masyvas.ndkiekis; j++){
-            if(Masyvas.ndrez[i]>Masyvas.ndrez[j])
-                swap(Masyvas.ndrez[i], Masyvas.ndrez[j]);
-        }
-    }
-}
 void isvedimas(vaikas Masyvas[], int n){
     cout << left << setw(15) << "Vardas" << left << setw(15) << "pavarde" << left << setw(15) <<"Galutinis (Vid.)" << " / " << setw(15) << "Galutinis (Med.)" << endl;
     for(int k=0; k<60; k++){
@@ -96,20 +92,17 @@ void isvedimas(vaikas Masyvas[], int n){
     cout << endl;
     for(int i=0; i<n; i++){
 
-        rusiavimas(Masyvas[i]);
+        sort(Masyvas[i].ndrez.begin(), Masyvas[i].ndrez.end());
         cout << left << setw(15) << Masyvas[i].vard << left << setw(15) << Masyvas[i].pav << left << setw(15) << fixed << setprecision(2) << vidurkis(sum(Masyvas[i].ndrez, Masyvas[i].ndkiekis), Masyvas[i].ndkiekis)<< left << setw(15) << median(Masyvas[i], Masyvas[i].ndkiekis) << endl;
     }
 }
 int main()
 {
     int n; // n - mokiniu skaicius, nd - namų darbų pažymys
-    vaikas A[10];
+    vaikas A[10]; // strukturos masyvas
     cout << "Iveskite vaiku kieki: ";
     cin >> n;
     ivedimas(A, n);
     isvedimas(A, n);
-    for(int i=0; i<n; i++){ //atlaisvinama atmintis
-        delete[] A[i].ndrez;
-    }
     return 0;
 }
