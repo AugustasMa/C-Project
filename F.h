c2db2133e07dbea7149519726320bc7c03e7c1de
AugustasMa <113709620+AugustasMa@users.noreply.github.com>
2022-11-15 15:00:14 +0200

    Add files via upload
    0.4 užduotis

---
+++ F.h
@@ -0,0 +1,40 @@
+#ifndef F_H_INCLUDED
+#define F_H_INCLUDED
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+#include <string>
+#include <chrono>
+#include <list>
+
+using namespace std;
+
+struct vaikas{
+    string vard;
+    string pav;
+    int egz; // egzamino rez.
+    vector<int> ndrez;// namų darbų rez.
+    double gp;
+};
+unsigned int Zodziai_eiluteje(std::string const& str);
+int random_pazymys();
+void ivedimas(vector<vaikas>& SK, int n);
+double vidurkis(double visindrez, int m); // vidurkio funkcija
+double median(vaikas SK, int n); // medianos funkcija
+double galutinis(double a, double b);
+void isvedimas(vector<vaikas> SK, int n);
+void failo_generavimas(vector<vaikas>& SK, int tempvaik);
+void skaitymas_is_failo_list (list<vaikas>& sk, int* kieknd, int tempvaik);
+void skaitymas_is_failo (vector<vaikas>& SK, int* kieknd, int tempvaik);
+void isvedimas_i_faila (vector<vaikas> SK, int kieknd);
+void isvedimas_i_faila_list (list<vaikas> SK, int kieknd);
+
+
+
+#endif // F_H_INCLUDED
